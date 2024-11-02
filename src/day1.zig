7eba5d68a21d8290b98c80e5dd00a756b276c604
TheAgaveFairy <93168751+TheAgaveFairy@users.noreply.github.com>
2024-11-02 00:13:37 -0500

    day one part one done!
    what a pain!!!!! i love it

--- src/day1.zig
+++ src/day1.zig
@@ -3,6 +3,15 @@ const ArrayList = std.ArrayList;
 const heap = std.heap;
 const printerr = std.debug.print;
 
+fn partOne(depths: []const usize) usize {
+    var answer: usize = 0;
+    for (depths[1..], 0..) |curr, i| {
+        const prev = depths[i];
+        if (curr > prev) answer += 1;
+    }
+    return answer;
+}
+
 pub fn main() !void {
     var gpa = heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
@@ -16,9 +25,6 @@ pub fn main() !void {
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-    try bw.flush(); // Don't forget to flush!
-
     var file = try std.fs.cwd().openFile("../inputs/day1.txt", .{});
     defer file.close();
 
@@ -26,14 +32,27 @@ pub fn main() !void {
     var in_stream = buf_reader.reader();
     var buf: [256]u8 = undefined; //ascii bytes
     
-    var depths = ArrayList(u32).init(allocator);
+    var depths = ArrayList(usize).init(allocator);
     defer depths.deinit();
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        const num = try std.fmt.parseInt(u32, line, 10);
+        const num = try std.fmt.parseInt(usize, line, 10);
         try depths.append(num);
     }
     
-    for (depths.items) |depth| {
-        try stdout.print("{}\n", .{depth});
+    var answer: usize = 0; // -1 accounts for depths[0] case returning "True"
+    //try stdout.print("0: {} BASE CASE", .{depths.items[0]});
+    for (depths.items[1..], 0..) |curr, i| {
+        const prev = depths.items[i];
+        //const gt = prev < curr;
+        if (prev < curr) answer += 1;
+        //try stdout.print("{}: {} {}\n", .{i+1, curr, gt});
     }
+    try bw.flush();
+
+    std.debug.print("Loop Done.\n", .{});
+    try stdout.print("Part One: Number Increasing: {}.\n", .{answer});
+    try bw.flush();
+
+    const p1 = partOne(depths.items);
+    std.debug.print("From fn(): {}.\n", .{p1});
 }
