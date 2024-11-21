6c2daead99198cb433c78c8ff1fe7c0c796a7a54
TheAgaveFairy <93168751+TheAgaveFairy@users.noreply.github.com>
2024-11-20 23:58:17 -0600

    sanity checking

--- src/day5.zig
+++ src/day5.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const printerr = std.debug.print;
 
-const N = 10;
+const N = 10; // 1000 or so for the real run
 
 const Direction = enum {
     horizontal,
@@ -12,13 +12,22 @@ const Direction = enum {
 const Coordinate = struct {
     x: usize,
     y: usize,
+
 };
+fn parseCoordinate(pair: []const u8) !Coordinate {
+    var pair_iter = std.mem.splitScalar(u8, std.mem.trim(u8, pair, " "), ',');
+    const x = pair_iter.next() orelse return error.InvalidFormat;
+    const y = pair_iter.next() orelse return error.InvalidFormat;
+    const int_x = try std.fmt.parseInt(usize, std.mem.trim(u8, x, " "), 10);
+    const int_y = try std.fmt.parseInt(usize, std.mem.trim(u8, y, " "), 10);
+    return Coordinate{.x = int_x, .y = int_y};
+}
 
 const Vent = struct {
     start: Coordinate,
     end: Coordinate,
 
-    pub fn getDirection(self: *Vent) Direction {
+    pub fn getDirection(self: Vent) Direction {
         if (self.start.x == self.end.x) return .vertical;
         if (self.start.y == self.end.y) return .horizontal;
         return .diagonal;
@@ -45,14 +54,14 @@ const Diagram = struct {
                 for (start..end+1) |y| self.grid[x][y] += 1;  
             },
 
-            .diagonal => {},
+            .diagonal => {}, // part 2
         }        
     }
 
     pub fn print(self: *Diagram) void {
         for (0..N) |x| {
             for (0..N) |y| {
-                printerr("{d:3}", .{self.grid[x][y]});
+                printerr("{d:5}", .{self.grid[x][y]});
             }
             printerr("\n", .{});
         }
@@ -63,22 +72,25 @@ fn readVents(allocator: std.mem.Allocator, reader: anytype) !std.ArrayList(Vent)
     var buf: [1024]u8 = undefined;
     
     var vent_list = std.ArrayList(Vent).init(allocator);
-    defer vent_list.deinit();    
 
     while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        var vent = Vent{ .start = Coordinate{ .x = 0, .y = 0}, .end = Coordinate{ .x = 1, .y = 1}};
-        vent.start.x = 0;
         var coords_iter = std.mem.splitSequence(u8, line, "->");
         const first_pair = coords_iter.next().?;
         const second_pair = coords_iter.next().?;
+        const start_coord: Coordinate = try parseCoordinate(first_pair);
+        const end_coord: Coordinate = try parseCoordinate(second_pair);
         printerr("First: {s}. Second: {s}\n", .{first_pair, second_pair});
+        const vent: Vent = Vent{
+            .start = start_coord,
+            .end = end_coord,
+        };
         try vent_list.append(vent);
     }
     return vent_list;
 }
 
 fn partOne(allocator: std.mem.Allocator) !usize {
-    const infilename = "../inputs/day5.txt";
+    const infilename = "../inputs/day5test.txt";
 
     var infile = try std.fs.cwd().openFile(infilename, .{});
     defer infile.close();
@@ -86,7 +98,10 @@ fn partOne(allocator: std.mem.Allocator) !usize {
     var buf_reader = std.io.bufferedReader(infile.reader());
     const instream = buf_reader.reader();
 
+    
     const vent_list = try readVents(allocator, instream);
+    defer _ = vent_list.deinit();
+    
     var diagram = Diagram{};
     for (vent_list.items) |vent| {
         diagram.markVent(vent);    
