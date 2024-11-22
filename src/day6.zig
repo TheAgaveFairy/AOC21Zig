4ea9ce5d90b9dc3a0864bf73dc3f248b8adf2b6f
TheAgaveFairy <93168751+TheAgaveFairy@users.noreply.github.com>
2024-11-21 21:08:53 -0600

    move file

--- src/day6.zig
+++ src/day6.zig
@@ -9,15 +9,22 @@ fn partOne(allocator: std.mem.Allocator) !usize {
     var buf_reader = std.io.bufferedReader(infile.reader());
     const instream = buf_reader.reader();
 
-    var initial_list = std.ArrayList(usize).init(allocator);
-    defer initial_list.deinit();
+    var fishes = std.ArrayList(usize).init(allocator);
+    defer fishes.deinit();
 
     var buf: [1024]u8 = undefined;
-    const line = try instream.readUntilDelimiterOrEof(&buf, '\n');
+    const line = try instream.readUntilDelimiterOrEof(&buf, '\n') orelse unreachable;
     const trimmed_line = std.mem.trim(u8, line, " ");
 
-    var input_iter = try std.mem.splitScalar(u8, trimmed_line, ',');
-    while (input_iter.next()) printerr("{d:3}", .{});
+    var input_iter = std.mem.splitScalar(u8, trimmed_line, ',');
+    while (input_iter.next()) |numstr| {
+        const num = try std.fmt.parseInt(usize, numstr, 10);
+        //printerr("{d:3}", .{num});
+        try fishes.append(num);
+    }
+
+    const len = fishes.items.len;
+    printerr("len {}", .{len});
 
     return 69;
 }
