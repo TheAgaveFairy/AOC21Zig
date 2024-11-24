aa2d7ac285fb97a7a75e1288f7a53670b1dfa4ac
TheAgaveFairy <93168751+TheAgaveFairy@users.noreply.github.com>
2024-11-23 20:51:30 -0600

    Update day8.zig

--- src/day8.zig
+++ src/day8.zig
@@ -3,6 +3,18 @@ const printerr = std.debug.print;
 
 const Display = struct {
     mapping: [7]u8,
+    const segments = [10][]const usize{
+        &[_]usize{ 0, 1, 2, 4, 5, 6 },
+        &[_]usize{ 2, 5 },
+        &[_]usize{ 0, 2, 3, 4, 6 },
+        &[_]usize{ 0, 2, 3, 5, 6 },
+        &[_]usize{ 1, 2, 3, 5 },
+        &[_]usize{ 0, 1, 3, 5, 6 },
+        &[_]usize{ 0, 1, 3, 4, 5, 6 },
+        &[_]usize{ 0, 2, 5 },
+        &[_]usize{ 0, 1, 2, 3, 4, 5, 6 },
+        &[_]usize{ 0, 1, 2, 3, 5, 6 },
+    };
 
     //  0000
     // 1    2
@@ -13,20 +25,58 @@ const Display = struct {
     //  6666
 
     pub fn setMapping(self: *Display, num_to_set: usize, chars: []u8) void {
-        const segments = [10][_]usize {
-            {0, 1, 2, 4, 5, 6},
-            {2, 5},
-            {0, 2, 3, 4, 6},
-            {0, 2, 3, 5, 6},
-            {1, 2, 3, 5},
-            {0, 1, 3, 5, 6},
-            {0, 1, 3, 4, 5, 6},
-            {0, 2, 5},
-            {0, 1, 2, 3, 4, 5, 6},
-            {0, 1, 2, 3, 5, 6},
-        };
-        std.debug.assert(chars.len == segments[num_to_set].len);
-        for (chars, segments[num_to_set]) |c, i| self.mapping[i] = c;
+        std.debug.assert(chars.len == self.segments[num_to_set].len);
+        for (chars, self.segments[num_to_set]) |c, i| self.mapping[i] = c;
+    }
+
+    fn inPattern(self: *Display, pattern: []usize, chars: []u8) bool {
+        for (pattern) |idx| { // example 1 gives 2 and 5, use with mapping to get their set chars
+            const key_char = self.mapping[idx];
+            const found = std.mem.containsAtLeast(u8, key_char, 1, chars);
+            if (!found) return false;
+        }
+        return true;
+    }
+
+    pub fn dealWithFives(self: *Display, fives: [3][5]u8) void {
+        // 2 3 5
+        // 2 and 3 differ on (4) vs (5). 5 only shares (0,3,6)
+
+        // 3 shares with 1 uniquely
+        for (fives) |pat| {
+            if (inPattern(segments[1], pat)) {
+                self.setMapping(3, pat);
+            }
+        }
+    }
+    pub fn dealWithSixes(self: *Display, sixes: [3][5]u8) void {
+        // 0 6 9
+        // 6 and 9 share (3) and differ on (2) vs (4).
+        for (sixes) |pat| {
+            if (inPattern(.{ 3, 4 }, pat)) { // this is boilerplate - NOT CORRECT NOR ROBUST AND IS ERROR-PRONE
+                self.setMapping(6, pat);
+            }
+        }
+    }
+
+    pub fn print(self: *Display) void {
+        const template =
+            \\  0000
+            \\ 1    2
+            \\ 1    2
+            \\  3333
+            \\ 4    5
+            \\ 4    5
+            \\  6666
+        ;
+        for (template) |c| {
+            if (std.ascii.isDigit(c)) {
+                const idx = try std.fmt.parseInt(u4, c, 10); // u4 to feel ~special
+                printerr("{c}", .{self.mapping[idx]});
+            } else {
+                printerr("{c}", .{c});
+            }
+        }
     }
 };
 
@@ -97,13 +147,41 @@ const Signal = struct {
 
     pub fn countUniques(self: *Signal) usize {
         var count: usize = 0;
-        //for (self.patterns) |p| {
-        //    if (isUnique(p)) count += 1;
-        //}
         for (self.outputs) |o| {
             if (isUnique(o)) count += 1;
         }
         return count;
+    }
+
+    pub fn buildDisplay(self: *Signal) Display {
+        var display: Display = undefined;
+
+        const fives = [3][5]u8; // @memcpy will mutate
+        const sixes = [3][6]u8; // @memcpy will mutate
+        var five_idx: usize = 0;
+        var six_idx: usize = 0;
+
+        for (self.patterns) |pat| {
+            switch (pat.len) { // the unique lens
+                2 => display.setMapping(1, pat),
+                3 => display.setMapping(7, pat),
+                4 => display.setMapping(4, pat),
+                7 => display.setMapping(8, pat),
+                5 => {
+                    @memcpy(fives[five_idx], pat);
+                    five_idx += 1;
+                },
+                6 => {
+                    @memcpy(sixes[six_idx], pat);
+                    six_idx += 1;
+                },
+                else => unreachable,
+            }
+        }
+        display.dealWithFives(fives);
+        display.dealWithSixes(sixes);
+        display.print();
+        return display;
     }
 };
 
@@ -139,6 +217,18 @@ fn partOne(allocator: std.mem.Allocator) !usize {
     return uniques;
 }
 
+fn partTwo(allocator: std.mem.Allocator) !usize {
+    var signals = try readFile(allocator);
+    defer signals.deinit();
+    defer for (signals.items) |*sig| sig.deinit();
+
+    for (signals.items) |*sig| {
+        const display = sig.buildDisplay();
+        display.print();
+    }
+    return 420;
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
@@ -146,4 +236,7 @@ pub fn main() !void {
 
     const part_one_answer = try partOne(allocator);
     printerr("Part One Answer: {}\n", .{part_one_answer});
+
+    const part_two_answer = try partTwo(allocator);
+    printerr("Part Two Answer: {}\n", .{part_two_answer});
 }
