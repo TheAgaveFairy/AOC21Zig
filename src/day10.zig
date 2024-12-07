325c307416f80d5ca836486ba8ff914182abb803
TheAgaveFairy <93168751+TheAgaveFairy@users.noreply.github.com>
2024-12-07 12:02:00 -0600

    day10pt2 switch bug

--- src/day10.zig
+++ src/day10.zig
@@ -15,6 +15,73 @@ inline fn getPair(char: u8) u8 {
     };
 }
 
+fn resolveStack(stack: []u8) !usize {
+    var answer: usize = 0;
+    for (stack) |char| {
+        const value = switch (char) {
+            '(' => 1,
+            '[' => 2,
+            '{' => 3,
+            '<' => 4,
+            else => unreachable,
+        };
+        answer *= 5;
+        answer += value;
+    }
+    return answer;
+}
+
+fn processLineTwo(allocator: std.mem.Allocator, line: []const u8) !usize {
+    var stack = std.ArrayList(u8).init(allocator);
+    defer stack.deinit();
+
+    for (line) |char| {
+        if (stack.items.len == 0) {
+            try stack.append(char);
+        } else {
+            const last = stack.getLast();
+            switch (char) {
+                ')' => {
+                    if (last == '(') {
+                        _ = stack.pop();
+                    } else {
+                        return 0;
+                    }
+                },
+                ']' => {
+                    if (last == '[') {
+                        _ = stack.pop();
+                    } else {
+                        return 0;
+                    }
+                },
+                '}' => {
+                    if (last == '{') {
+                        _ = stack.pop();
+                    } else {
+                        return 0;
+                    }
+                },
+                '>' => {
+                    if (last == '<') {
+                        _ = stack.pop();
+                    } else {
+                        return 0;
+                    }
+                },
+                else => {
+                    try stack.append(char);
+                },
+            }
+        }
+    }
+    printerr("Stack: ", .{});
+    for (stack.items) |s| printerr("{c}", .{s});
+    printerr("\n", .{});
+
+    return try resolveStack(stack.items);
+}
+
 fn processLine(allocator: std.mem.Allocator, line: []const u8) !usize {
     var stack = std.ArrayList(u8).init(allocator);
     defer stack.deinit();
@@ -63,11 +130,12 @@ fn processLine(allocator: std.mem.Allocator, line: []const u8) !usize {
                 },
             }
         }
-        //printerr("Stack: ", .{});
-        //for (stack.items) |s| printerr("{c}", .{s});
-        //printerr("\n", .{});
     }
     //printerr("\n\n\n", .{});
+    printerr("Stack: ", .{});
+    for (stack.items) |s| printerr("{c}", .{s});
+    printerr("\n", .{});
+
     return 0;
 }
 
@@ -81,17 +149,27 @@ fn partOne(allocator: std.mem.Allocator, contents: []u8) !usize {
     return answer;
 }
 
+fn partTwo(allocator: std.mem.Allocator, contents: []u8) !usize {
+    var lines = std.mem.splitScalar(u8, contents, '\n');
+    var answer: usize = 0;
+    while (lines.next()) |line| {
+        //printerr("{s}\n", .{line});
+        answer += try processLineTwo(allocator, line);
+    }
+    return answer;
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    const filename = "../inputs/day10.txt";
+    const filename = "../inputs/day10test.txt";
     const content: []u8 = try std.fs.cwd().readFileAlloc(allocator, filename, std.math.maxInt(usize));
     defer allocator.free(content);
 
     const part_one_answer = try partOne(allocator, content);
     printerr("Part One Answer: {}\n", .{part_one_answer});
-    //const part_two_answer = try partTwo(allocator, filename);
-    //printerr("Part Two Answer: {}\n", .{part_two_answer});
+    const part_two_answer = try partTwo(allocator, content);
+    printerr("Part Two Answer: {}\n", .{part_two_answer});
 }
